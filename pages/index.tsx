682d49860e8e19c68aca6ffcf661de14c2fd633d
Romelianism <romelianism@gmail.com>
2022-08-18 23:08:34 +0800

    feat: add love cubes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
-import { OrbitControls, TransformControls } from "@react-three/drei";
+import { OrbitControls, Text } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useState } from "react";
 import styles from "../styles/Home.module.css";
+import randomOnUnitSphere from "../utils/randomOnUnitSphere";
 
 const Home: NextPage = () => {
+  const [test, setTest] = useState<boolean>(false);
   return (
     <div className={styles.container}>
       <Head>
@@ -15,24 +18,48 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Canvas shadows={true}>
-        <pointLight castShadow={true} position={[10, 10, 10]} />
-        <mesh
-          receiveShadow={true}
-          scale={100}
-          position={[0, -5, 0]}
-          rotation={[-Math.PI / 2, 0, 0]}
+      <Canvas>
+        <color attach="background" args={[0xfff2f1]} />
+        <pointLight />
+        {[...Array(256)].map((x, i) => {
+          const topScale = 16;
+          const scale = Math.random() * topScale;
+          return (
+            <mesh
+              scale={scale}
+              position={
+                randomOnUnitSphere().map(
+                  (x) => x * (5 + scale + Math.random() * topScale)
+                ) as [x: number, y: number, z: number]
+              }
+            >
+              <boxGeometry />
+              <meshToonMaterial
+                color={
+                  [0xff5c58, 0xfe8f8f, 0xfcd2d1, 0xffedd3][
+                    Math.floor(Math.random() * 4)
+                  ]
+                }
+              />
+            </mesh>
+          );
+        })}
+        <Text
+          fontSize={0.6}
+          color="white"
+          onClick={() => {
+            setTest(test);
+            console.log(test);
+          }}
         >
-          <planeGeometry />
-          <meshStandardMaterial />
-        </mesh>
-        <TransformControls mode="translate">
-          <mesh castShadow={true} scale={1} position={[0, 0, 0]}>
-            <sphereGeometry />
-            <meshStandardMaterial />
-          </mesh>
-        </TransformControls>
-        <OrbitControls makeDefault />
+          Hello Love!
+        </Text>
+        <OrbitControls
+          makeDefault
+          position={[0, 0, 0]}
+          enableZoom={false}
+          enablePan={false}
+        />
       </Canvas>
     </div>
   );
